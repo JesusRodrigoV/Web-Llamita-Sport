906a9a34dc51b367d9d85fd0d1cfe28a97d5aa5c
JesusRodrigoV <jaicelvelasco105@gmail.com>
2024-11-20 21:28:35 -0400

    Creacion de componentes de la pagina

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component'
+import { HomeComponent } from './home/home.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'inventario', component: InventarioComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'contactos', component: ContactosComponent },
+];
